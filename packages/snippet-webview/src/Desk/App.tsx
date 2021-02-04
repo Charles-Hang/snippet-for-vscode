5238ccc3efbe30ef4037508841256f4122d26559
Shiwuxing Hang <hangshiwuxing@itiger.com>
2021-02-04 20:47:01 +0800

    feat: desk deskView双入口配置

---
+++ packages/snippet-webview/src/Desk/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+    return <div>desk webview</div>;
+}
+
+export default App;
---
+++ packages/snippet-webview/src/DeskView/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+    return <div>desk view webview</div>;
+}
+
+export default App;
